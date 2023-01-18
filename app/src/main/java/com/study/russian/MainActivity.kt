bb91f5cc70d806e71ac2ecd16266d909b98f93f0
DAngelo-Diego <dangelodiego967@gmail.com>
2023-01-18 20:20:07 -0300

    Firebase Analytics & Crashlytics Added

--- app/src/main/java/com/study/russian/MainActivity.kt
+++ app/src/main/java/com/study/russian/MainActivity.kt
@@ -2,6 +2,7 @@ package com.study.russian
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
